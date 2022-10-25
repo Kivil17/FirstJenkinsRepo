6d0ce1169b9419d8a6353afa50cdd6f0a98b823a
Kivil17 <58693503+Kivil17@users.noreply.github.com>
2022-10-25 12:59:55 +0200

    Update aws-instance-example.tf

--- Terraform-Test-master/aws-instance-first-script/aws-instance-example.tf
+++ Terraform-Test-master/aws-instance-first-script/aws-instance-example.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "web1" {
    ami           = "${lookup(var.ami_id, var.region)}"
    instance_type = "t2.micro"
    vpc_security_group_ids = ["${aws_security_group.webSG.id}"]
-   key_name = "myKeyPair"
+   key_name = "myKeyPairPem"
    
 
     tags = {
